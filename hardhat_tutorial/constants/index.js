666ee478e688afbc18a4ba3c2da2ff02acc8caf7
Rik Schoonbeek <rik.schoonbeek@gmail.com>
2022-12-04 15:12:12 +0100

    First commit

---
+++ hardhat_tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x185B38D64d3716e62B01Dd9558E9c798fFeE2770";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
